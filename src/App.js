ebab2f6a1c456192128d769c0985b9a84f6e566d
GianlucaTravasci <gianlucatravasci@gmail.com>
2020-07-24 10:16:44 +0200

    Added the style to the app and the json call for the robots

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import React, {Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
+import './App.css';
 
 class App extends Component {
     constructor () {
         super()
-        this.state = {
-            robots: robots,
+        this.state = { // You have to put in state somethin that chang and modify the app, props are those values that are always the same.
+            robots: [],
             searchField: ''
         }
     }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(user => this.setState({robots: user}));
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchField: event.target.value});
+    }
     render() {
+        const filteredRobots = this.state.robots.filter(robots => {
+            return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+        })
         return (
-            <div>
-                <h1>RoboFriends</h1>
-                <SearchBox />
-                <CardList robots = {robots}/>
+            <div className='tc'>
+                <h1 className= 'f1'>RoboFriends</h1>
+                <SearchBox searchChange = {this.onSearchChange}/>
+                <CardList robots = {filteredRobots}/>
             </div>
         )
     }
