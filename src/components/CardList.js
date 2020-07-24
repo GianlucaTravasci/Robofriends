4cccd8ef84da21347f6ddd606c8cf3c3d1dab442
GianlucaTravasci <gianlucatravasci@gmail.com>
2020-07-24 13:32:17 +0200

    added the error boundler

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({robots}) => {
+    /*if (true) { Checking the ErrorBoundary
+        return new Error("nooo");
+    }*/
     const robotsArray = robots.map((user, i) => {
         return (
             <Card
