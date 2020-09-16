579b4695ecfab4288d023c5fb182ae034bf971fa
GianlucaTravasci <gianluca.travasci@gmail.com>
2020-09-16 15:09:50 +0200

    finished the app with only react, todo: redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,28 +1,45 @@
-import React, {Component} from 'react';
-import Card from '../components/Card';
-
-//import CardList from '../components/CardList';
-//import SearchBox from '../components/SearchBox';
-//import Scroll from '../components/Scroll';
-//import ErrorBoundary from '../components/ErrorBoundary'
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
 import './App.css';
 
-import {robots} from '../robots'
-
 class App extends Component {
-    render() {
-        return(          
-            <div>
-                <div className="tc">
-                    <h1>Helloo!!</h1>
-                </div>
-                {robots.map(({ id, name, email }) => {
-                    return (<Card id={id} name={name} email={email}/>)
-                })}
-            </div>
-        )
+  constructor() {
+    super()
+    this.state = {
+      robots: [],
+      searchfield: ''
     }
-    
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response=> response.json())
+      .then(users => {this.setState({ robots: users})});
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value })
+  }
+
+  render() {
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter(robot =>{
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    })
+    return !robots.length ?
+      <h1>Loading</h1> :
+      (
+        <div className='tc'>
+          <h1 className='f1'>RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange}/>
+          <Scroll>
+            <CardList robots={filteredRobots} />
+          </Scroll>
+        </div>
+      );
+  }
 }
 
 export default App;
