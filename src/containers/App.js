0700fe1d10929c4d2c67fa22f55219c3727dbb8d
GianlucaTravasci <gianluca.travasci@gmail.com>
2020-09-16 11:55:58 +0200

    delete everthing to restart

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,37 +6,10 @@ import ErrorBoundary from '../components/ErrorBoundary'
 import './App.css';
 
 class App extends Component {
-    constructor () {
-        super()
-        this.state = { // You have to put in state somethin that chang and modify the app, props are those values that are always the same.
-            robots: [],
-            searchField: ''
-        }
-    }
-
-    componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response => response.json())
-        .then(user => this.setState({robots: user}));
-    }
-
-    onSearchChange = (event) => {
-        this.setState({searchField: event.target.value});
-    }
     render() {
-        const { robots, searchField } = this.state;
-        const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchField.toLowerCase());
-        })
-        return (
-            <div className='tc'>
-                <h1 className= 'f1'>RoboFriends</h1>
-                <SearchBox searchChange = {this.onSearchChange}/>
-                <Scroll>
-                    <ErrorBoundary>
-                        <CardList robots = {filteredRobots}/>
-                    </ErrorBoundary>
-                </Scroll>
+        return(
+            <div className="tc pa4">
+                <h1><strng>Hello!</strng></h1>
             </div>
         )
     }
