d0bf577f8656628286ab199dd8babe57377b5b33
GianlucaTravasci <gianluca.travasci@gmail.com>
2020-09-18 15:43:59 +0200

    some other fix from lighthouse report

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,13 +4,11 @@ const SearchBox = ({ searchfield, searchChange }) => {
 
     return (
       <div className='pa2'>
-        <label htmlFor="searchBox" className="pa3">Search:</label>
         <input
+          aria-label='search robots'
           className='pa3 ba b--green bg-lightest-blue'
           type='search'
           placeholder='search robots'
-          id="searchBox"
-          className="searchBox"
           onChange={searchChange}
         />
       </div>
