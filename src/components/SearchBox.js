28bb33fcabacc370774519abe36b8d5f56987c3a
GianlucaTravasci <gianluca.travasci@gmail.com>
2020-09-18 15:38:22 +0200

    test

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,11 +4,13 @@ const SearchBox = ({ searchfield, searchChange }) => {
 
     return (
       <div className='pa2'>
-        <label className="pa3">Search:</label>
+        <label htmlFor="searchBox" className="pa3">Search:</label>
         <input
           className='pa3 ba b--green bg-lightest-blue'
           type='search'
           placeholder='search robots'
+          id="searchBox"
+          className="searchBox"
           onChange={searchChange}
         />
       </div>
