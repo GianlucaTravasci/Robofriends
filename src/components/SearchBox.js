01fb4c7171ce53d22aee08cb3f1d00b2002f8d50
GianlucaTravasci <gianluca.travasci@gmail.com>
2020-09-18 15:24:29 +0200

    a little accessibility feature

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,6 +4,7 @@ const SearchBox = ({ searchfield, searchChange }) => {
 
     return (
       <div className='pa2'>
+        <label className="pa3">Search:</label>
         <input
           className='pa3 ba b--green bg-lightest-blue'
           type='search'
