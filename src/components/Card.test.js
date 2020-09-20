3f34bb33be08332b8bfb1fb076958c3132bae308
GianlucaTravasci <gianluca.travasci@gmail.com>
2020-09-20 11:38:33 +0200

    simple shallow test

---
+++ src/components/Card.test.js
@@ -0,0 +1,10 @@
+import { shallow, mount, render } from 'enzyme';
+import React from 'react'
+
+import Card from './Card';
+
+
+it('expecto render car component', () => {
+    expect(shallow(<Card/>).length).toEqual(1)
+
+})
