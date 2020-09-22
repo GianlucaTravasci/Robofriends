19c81d45692b86fa5086b3d926acc68b760dc674
GianlucaTravasci <gianluca.travasci@gmail.com>
2020-09-22 13:37:21 +0200

    tests done for actions and reducers

--- src/reducers.js
+++ src/reducers.js
@@ -20,9 +20,9 @@ export const searchRobots = (state = initialStateSearch, action = {}) => {
 } 
 
 const initialStateRobots = {
-    isPending: false,
+    isPending: true,
     robots: [],
-    error: ''
+    //error: ''
 }
 
 export const requestRobots = (state = initialStateRobots, action = {}) => {
@@ -32,7 +32,7 @@ export const requestRobots = (state = initialStateRobots, action = {}) => {
         case REQUEST_ROBOTS_SUCCESS:
             return Object.assign({}, state, {robots: action.payload, isPending: false});
         case REQUEST_ROBOTS_FAILED:
-            return Object.assign({}, state, {error: action.payload, isPending: false});
+            return Object.assign({}, state, {error: action.payload});
         default:
             return state;
     }
